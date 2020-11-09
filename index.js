9acc5499f749089d7a4a0029f8a7cfd219b9b280
areum514 <21500514@handong.edu>
2020-11-09 23:12:58 +0900

    settting 2

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "areum", age = 25, gender = "f";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
