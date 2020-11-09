12c9e1b6077df2df1e1c132cb4cfbb9f25e58d2e
areum514 <21500514@handong.edu>
2020-11-09 23:26:15 +0900

    class on ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human{
-    name:string;
-    age:number;
-    gender:string;
+//class로 작성하면 index.js에 class를 볼 수 있음 interface로 작성하면 아무것도 안나오지
+class Human{
+    public name:string;
+    public age:number;
+    public gender:string;
+    constructor(name:string,age:number,gender?:string){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
 }
-
-const person={
-    name="areum",
-    age=25,
-    gender="f"
-}
+const areum=new Human("areum",25,"f")
 
 const sayHi=(person:Human):void=>{
     console.log(`Hello ${person.name}, you are ${person.age}, you are ${person.gender}`);
 }
 
-sayHi(person)
+sayHi(areum)
 
 export{}
